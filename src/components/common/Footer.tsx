aceab3d6b9b15ab45aba13e6127d1053ffa42e8b
태의 <ted.kordev@gmail.com>
2024-12-29 19:07:49 +0900

    fix: privacy

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -20,10 +20,13 @@ export default function Footer() {
           {"."}
         </Typography>
         <Box sx={{ display: "flex", justifyContent: "center", gap: 2, mt: 2 }}>
-          <Link color="inherit" href="https://berakorean.com/privacy" target="_blank" rel="noopener noreferrer">
+          {/* <Link color="inherit" href="https://berakorean.com/privacy" target="_blank" rel="noopener noreferrer">
             개인정보처리방침
           </Link>
           <Link color="inherit" href="http://beko-privacy.s3-website.ap-northeast-2.amazonaws.com" target="_blank" rel="noopener noreferrer">
+            개인정보처리방침
+          </Link> */}
+          <Link color="inherit" href="https://flame-mascara-204.notion.site/Policy-15da507c1a62806aa0cbe24c8769c536" target="_blank" rel="noopener noreferrer">
             개인정보처리방침
           </Link>
           <RouterLink to="/terms/marketing" style={{ color: "inherit", textDecoration: "none" }}>
